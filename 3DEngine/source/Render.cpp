38314e51391b7d03d336dd2a9c4a41c94fa73567
Coleman Jonas <junkbot135@gmail.com>
2019-07-19 18:15:44 -0700

    slightly better

--- 3DEngine/source/Render.cpp
+++ 3DEngine/source/Render.cpp
@@ -1282,9 +1282,15 @@ GLuint Render::LoadHDRimage(std::string filename, bool irr)
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 5);
+  //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_LOD, -100);
+  //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LOD, 100);
+
+  //glBindTexture(GL_TEXTURE_2D, 0);
   glGenerateMipmap(GL_TEXTURE_2D);
+  //glGenerateTextureMipmap(newTexture);
   return newTexture;
 }
 
